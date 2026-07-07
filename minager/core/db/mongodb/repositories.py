2724beb109ad6530bda9e822da16ecee812aa437
Su1tan <46191469+SultanNasyrovDeveloper@users.noreply.github.com>
2026-07-07 21:54:06 +0300

    Fix model class type on a mongo db model.

--- minager/core/db/mongodb/repositories.py
+++ minager/core/db/mongodb/repositories.py
@@ -5,23 +5,23 @@ from pymongo import ReturnDocument
 from .models import MongoDBModel
 
 
-class MongoDBRepository[Model: MongoDBModel]:
+class MongoDBRepository[ModelT: MongoDBModel]:
     COLLECTION: str
-    model_class: Model
+    model_class: type[ModelT]
 
     def __init__(self, connection: motor.AsyncIOMotorCollection):
         self.connection = connection
 
-    async def get(self, id_: str | ObjectId) -> Model | None:
+    async def get(self, id_: str | ObjectId) -> ModelT | None:
         id_ = id_ if isinstance(id_, ObjectId) else ObjectId(id_)
         session_data = await self.connection.find_one({'_id': id_})
         return self.model_class.model_validate(session_data) if session_data else None
 
-    async def create(self, data: dict) -> Model | None:
+    async def create(self, data: dict) -> ModelT | None:
         insert_result = await self.connection.insert_one(data)
         return await self.get(insert_result.inserted_id)
 
-    async def update(self, id_: str | ObjectId, data: dict) -> Model:
+    async def update(self, id_: str | ObjectId, data: dict) -> ModelT:
         id_ = id_ if isinstance(id_, ObjectId) else ObjectId(id_)
         updated = await self.connection.find_one_and_update(
             {'_id': id_}, {'$set': data}, return_document=ReturnDocument.AFTER
